100cb8cc1fd7da8c0082b6b6231f3fb748e3f8f4
unlocomqx <amxmodx.unloco@gmail.com>
2017-02-27 15:55:08 +0100

    install ng gh-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
+import { SliderComponent } from "./slider/slider.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
